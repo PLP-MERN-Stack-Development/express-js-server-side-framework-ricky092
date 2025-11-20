ae3712c5842f55450a68c1bc006b248720ad1412
ricky092 <lamiodun8@gmail.com>
2025-11-20 15:50:48 +0100

    Add nested repo as normal folder

---
+++ express-js-server-side-framework-ricky092/middleware/utils/errors.js
@@ -0,0 +1,18 @@
+class NotFoundError extends Error {
+    constructor(message) {
+        super(message);
+        this.statusCode = 404;
+    }   
+}
+
+class BadRequestError extends Error {
+    constructor(message) {
+        super(message);
+        this.statusCode = 400;
+    }   
+}
+
+module.exports = {
+    NotFoundError,
+    BadRequestError
+};
